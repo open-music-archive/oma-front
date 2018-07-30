2bfcdf12d5ac7f4965dea91a0a502bbabf8d0ad8
florianthalmann <florianthalmann@bluewin.ch>
2018-07-30 22:54:46 +0900

    added live textures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,32 @@ import { ApiService } from './services/api-service';
 })
 export class AppComponent implements OnInit {
 
+  private player: DymoPlayerManager;
   private texture: string;
+  private previousUri: string;
 
   constructor(private apiService: ApiService) {}
 
   async ngOnInit() {
-    this.texture = await this.apiService.getTexture();
+    this.player = new DymoPlayerManager(true, true, 0.2, 2, 0.04);
+    await this.player.init("https://raw.githubusercontent.com/dynamic-music/dymo-core/master/ontologies/");
+    this.playLiveTexture();
+  }
 
-    const player = new DymoPlayerManager(true, true, 0.2, 2);
-    await player.init("https://raw.githubusercontent.com/dynamic-music/dymo-core/master/ontologies/");
-    console.log("loaded", await player.getDymoManager().getStore().size());
-    await player.loadDymoFromString(this.texture);
-    player.startPlaying();
+  private playLiveTexture() {
+    this.apiService.onLiveTexture().subscribe(async texture => {
+      this.texture = texture;
+      const newUri = (await this.player.loadDymoFromString(texture)).dymoUris;
+      console.log("LIVE", newUri, this.previousUri);
+      this.player.startPlayingUri(newUri[newUri.length-1], this.previousUri);
+      this.previousUri = newUri[newUri.length-1];
+    })
+  }
+
+  private async playTexture() {
+    this.texture = await this.apiService.getTexture();
+    await this.player.loadDymoFromString(this.texture);
+    this.player.startPlaying();
   }
 
 }
--- src/app/services/api-service.ts
+++ src/app/services/api-service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import * as socketIO from 'socket.io-client';
 
 @Injectable()
 export class ApiService {
 
-  private API_URL = "https://play-it-again-use-it-together.herokuapp.com/";//"http://localhost:8060/";
+  private API_URL = "http://localhost:8060/"//"https://play-it-again-use-it-together.herokuapp.com/";//"http://localhost:8060/";
+  private socket = socketIO(this.API_URL, {
+    extraHeaders: {
+      'Access-Control-Allow-Credentials': 'omit'
+    }
+  });
 
   async getTexture(): Promise<string> {
     return JSON.stringify(await this.getJsonFromApi('texture/'), null, "    ");
+  }
+
+  onLiveTexture(): Observable<string> {
+    return new Observable<string>(observer => {
+      this.socket.on('live-stream', (data: string) => observer.next(data));
+    });
   }
 
   private async getJsonFromApi(path: string, params?: {}): Promise<{}> {
