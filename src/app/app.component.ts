c77a0b45ed69f7615e3d071ea6b6ee3bcbda0d38
florianthalmann <florianthalmann@bluewin.ch>
2018-09-09 00:13:20 +0100

    updated dependencies, new player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,27 @@ export class AppComponent implements OnInit {
   private player: DymoPlayer;
   private texture: string;
   private previousUri: string;
+  private playingUris: number;
 
   constructor(private apiService: ApiService) {}
 
   async ngOnInit() {
-    this.player = new DymoPlayer(true, true, 0.2, 2, 0.04);
+    this.player = new DymoPlayer({useWorkers:true, preloadBuffers:true,
+      scheduleAheadTime:1, loadAheadTime:1, loggingOn:true});
     await this.player.init("https://raw.githubusercontent.com/dynamic-music/dymo-core/master/ontologies/");
+    this.player.getPlayingDymoUris().subscribe(ds => this.playingUris = ds.length);
     this.playLiveTexture();
   }
 
   private playLiveTexture() {
     this.apiService.onLiveTexture().subscribe(async texture => {
       this.texture = texture;
-      const newUri = (await this.player.loadDymoFromString(texture)).dymoUris;
+      const newUris = (await this.player.loadDymoFromString(texture)).dymoUris;
+      const newUri = newUris[newUris.length-1];
       console.log("LIVE", newUri, this.previousUri);
-      this.player.playUri(newUri[newUri.length-1], this.previousUri);
-      this.previousUri = newUri[newUri.length-1];
+      this.player.playUri(newUri);//, this.previousUri);
+      //this.player.transitionToUri(newUri, this.previousUri, 5);
+      this.previousUri = newUri;
     })
   }
 
