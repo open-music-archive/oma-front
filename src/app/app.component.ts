3521757cb773225b2a88118f61329017db414cbe
florianthalmann <florianthalmann@bluewin.ch>
2018-09-09 21:33:06 +0100

    added performance info and transitions work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,18 @@ export class AppComponent implements OnInit {
   private player: DymoPlayer;
   private texture: string;
   private previousUri: string;
-  private playingUris: number;
+  private numPlayingDymos: number;
+  private numLoadedBuffers: number;
+  private performanceInfo: string;
 
   constructor(private apiService: ApiService) {}
 
   async ngOnInit() {
     this.player = new DymoPlayer({useWorkers:true, preloadBuffers:true,
-      scheduleAheadTime:1, loadAheadTime:1, loggingOn:true});
+      scheduleAheadTime:1, loadAheadTime:1, loggingOn: false});
     await this.player.init("https://raw.githubusercontent.com/dynamic-music/dymo-core/master/ontologies/");
-    this.player.getPlayingDymoUris().subscribe(ds => this.playingUris = ds.length);
+    this.player.getPlayingDymoUris().subscribe(ds => this.numPlayingDymos = ds.length);
+    this.player.getAudioBank().getBufferCount().subscribe(n => this.numLoadedBuffers = n);
     this.playLiveTexture();
   }
 
@@ -29,9 +32,10 @@ export class AppComponent implements OnInit {
       this.texture = texture;
       const newUris = (await this.player.loadDymoFromString(texture)).dymoUris;
       const newUri = newUris[newUris.length-1];
-      console.log("LIVE", newUri, this.previousUri);
-      this.player.playUri(newUri);//, this.previousUri);
-      //this.player.transitionToUri(newUri, this.previousUri, 5);
+      console.log("LIVE", await this.player.getDymoManager().getStore().size());
+      this.updatePerformanceInfo();
+      //this.player.playUri(newUri);//, this.previousUri);
+      this.player.transitionToUri(newUri, this.previousUri, 3);
       this.previousUri = newUri;
     })
   }
@@ -40,6 +44,16 @@ export class AppComponent implements OnInit {
     this.texture = await this.apiService.getTexture();
     await this.player.loadDymoFromString(this.texture);
     this.player.play();
+  }
+
+  private async updatePerformanceInfo() {
+    let info: string[] = [];
+    const store = this.player.getDymoManager().getStore();
+    info.push("triples: " + await store.size());
+    info.push("observers: " + await store.getValueObserverCount());
+    info.push("dymos: " + this.numPlayingDymos);
+    info.push("buffers: " + this.numLoadedBuffers);
+    this.performanceInfo = info.join(', ');
   }
 
 }
