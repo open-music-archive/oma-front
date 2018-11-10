6ccf34a10366b6cc4dce47667f9dc2183129e8f7
florianthalmann <florianthalmann@bluewin.ch>
2018-11-10 16:44:27 +0000

    keypress works

--- src/app/mpc.component.ts
+++ src/app/mpc.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService, SoundObject } from './services/api-service';
 
+const KEYS = [[49,50,51,52],[81,87,69,82],[65,83,68,70],[89,88,67,86]];
+
 @Component({
   templateUrl: './mpc.component.html'
 })
@@ -17,6 +19,11 @@ export class MpcComponent {
 
   play(sound: SoundObject) {
     new Audio(sound.audioUri).play();
+  }
+
+  keyDown(event: KeyboardEvent) {
+    KEYS.forEach((row,i) => row.forEach((val,j) =>
+      event.keyCode == val ? this.play(this.sounds[i*4+j]) : null));
   }
 
 }
